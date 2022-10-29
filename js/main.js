d41c06238fb7149b91b6be3a7488de234fd4fa5c
Dearian <dearian2706@gmail.com>
2022-10-29 23:47:51 +0300

    segment escape

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const task = document.querySelector('.text');
 const canvas = document.querySelector('.canvas');
 const circles = document.querySelector('.circles');
 const lines = document.querySelector('.lines');
-
-const objects = [];
-const segments = [];
+console.log(lines)
+let objects = [];
+let segments = [];
 
 let stage = "objects"; // objects | ribs | completion 
 let currentObject = null; // chosen object as start pos
@@ -19,7 +19,6 @@ const taskTexts = {
 }
 task.innerHTML = taskTexts[stage];
 
-
 addEventListener('click', (event) => {
   if (event.target === next) {
     stage = "objects" ? "ribs" : "ribs" && "completion";
@@ -28,49 +27,4 @@ addEventListener('click', (event) => {
   }
 })
 
-
-const segment = document.querySelector('.line');
-
-addEventListener('mousemove', (event) => {
-  if (stage == 'ribs' && !ribDirecting) {
-    let isSet = false;
-    for (obj of objects) {
-      if (obj.cx.animVal.value - event.clientX < 20 &&
-        obj.cx.animVal.value - event.clientX > -20 &&
-        obj.cy.animVal.value - event.clientY < 20 &&
-        obj.cy.animVal.value - event.clientY > -20
-      ) {
-        isSet = true;
-        segment.setAttribute("x2", obj.cx.animVal.value);
-        segment.setAttribute("y2", obj.cy.animVal.value);
-      }
-    }
-    if (!isSet) {
-      segment.setAttribute("x2", event.clientX);
-      segment.setAttribute("y2", event.clientY);
-      isSet = false
-    }
-  }
-
-});
-
-
-
-let scheme = [
-  [0, 8, 0, 0, 2, 0],
-  [8, 0, 9, 0, 4, 3],
-  [0, 9, 0, 8, 0, 5],
-  [0, 8, 8, 0, 0, 10],
-  [2, 4, 0, 6, 0, 0],
-  [0, 3, 5, 10, 0, 0],
-];
-let result = [
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0]
-];
-let way = [0];
 
